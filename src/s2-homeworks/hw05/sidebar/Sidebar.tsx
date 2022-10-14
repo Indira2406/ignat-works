59458b1f06fdf25a50290ed2bce3fddc956421e8
iagzamova <indira.agzamova@ps.kz>
2022-10-14 20:22:05 +0600

    string instead of undefined

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -28,7 +28,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-pre-junior-link"}
             to={PATH.PRE_JUNIOR}
             onClick={handleClose}
-            className={({ isActive }) => (isActive ? s.active : undefined)} // делает студент
+            className={({ isActive }) => (isActive ? s.active : '')} // делает студент
           >
             Pre-junior
           </NavLink>
@@ -36,9 +36,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-junior-link"}
             to={PATH.JUNIOR}
             onClick={handleClose}
-            className={({ isActive }) =>
-              isActive ? s.active : undefined
-            } // делает студент
+            className={({ isActive }) => isActive ? s.active : ''} // делает студент
           >
             Junior
           </NavLink>
@@ -46,9 +44,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-junior-plus-link"}
             to={PATH.JUNIOR_PLUS}
             onClick={handleClose}
-            className={({isActive}) =>
-              isActive ? s.active : undefined
-            } // делает студент
+            className={({isActive}) =>  isActive ? s.active : ''} // делает студент
           >
             Junior Plus
           </NavLink>
